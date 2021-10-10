58ca2abbf97855e2f497c841cf8d84f69a35e407
abritopach <adrbrpa1988@gmail.com>
2021-10-10 11:01:17 +0100

    ✨ Added tab model

---
+++ lib/data/models/tab.model.dart
@@ -0,0 +1,11 @@
+class Tab {
+  // Store index and name of the tab.
+  final int index;
+  final String title;
+  // Create a const constructor with both fields as required and add normal assertions for safety.
+  const Tab({
+    required this.index,
+    required this.title,
+  })  : assert(index > 0, 'index cannot be negative'),
+        assert(title != null, 'title cannot be null');
+}
